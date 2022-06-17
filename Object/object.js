e8cf53bd7d7de9809acd21790db89993a80467df
gauravgupta <grkml9630@gmail.com>
2022-06-17 15:42:56 +0530

    objectType

---
+++ Object/object.js
@@ -0,0 +1,12 @@
+var person1 = {
+    name: 'John',
+    age: 36,
+    email: 'john@example.com'
+};
+console.log(person1.name);
+var person2 = {
+    name: 'Sam',
+    age: 30,
+    email: 'sam@example.com'
+};
+console.log(person2.name);
