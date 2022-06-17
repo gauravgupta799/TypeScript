17e6bb7761dd9970e4987761ce9f8a05d158a320
gauravgupta <grkml9630@gmail.com>
2022-06-17 15:41:51 +0530

    objectType

---
+++ Object/object.ts
@@ -0,0 +1,17 @@
+type objPerson = {name:string, age:number,email:string}
+
+const person1:objPerson= {
+    name: 'John',
+    age: 36,
+    email: 'john@example.com'
+}
+console.log(person1.name);
+
+
+const person2:objPerson = {
+    name: 'Sam',
+    age: 30,
+    email: 'sam@example.com'
+}
+
+console.log(person2.name);
