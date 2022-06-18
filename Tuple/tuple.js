99d0502102755cda9231b363071eaeb3f2fe70df
gauravgupta <grkml9630@gmail.com>
2022-06-18 19:32:55 +0530

    tuple

---
+++ Tuple/tuple.js
@@ -0,0 +1,6 @@
+// A tuple is a typed array with a predefined length and types for each index.
+// Tuples are great because they allow each element in the array to be a known typre of value.
+var myTuple;
+myTuple = [8, false, "Coding"];
+myTuple.push('Something new and wrong');
+console.log("MyTuple", myTuple);
