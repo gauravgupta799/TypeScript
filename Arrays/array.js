94ff904eff05cadbc95e49774c67c3749e8c2c10
gauravgupta <grkml9630@gmail.com>
2022-06-18 18:48:45 +0530

    arrays

---
+++ Arrays/array.js
@@ -0,0 +1,6 @@
+var arr = ["Java", "php", "javascript", "pyhton", "node"];
+arr.push("Swift");
+console.log(arr);
+// >>> Way to define any type of array
+// let arr: any[] = ["Java", "php","javascript",2334,"node", true];
+// let Arr = ["Java", "php","javascript","pyhton","node",3453,false,843.43];
---
+++ Arrays/array.ts
@@ -0,0 +1,8 @@
+let arr:string[] = ["Java", "php","javascript","pyhton","node"];
+arr.push("Swift");
+console.log(arr);
+
+// >>> Way to define any type of array
+// let arr: any[] = ["Java", "php","javascript",2334,"node", true];
+
+// let Arr = ["Java", "php","javascript","pyhton","node",3453,false,843.43];
