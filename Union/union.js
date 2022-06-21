faf3d4237255789fed921bd4ff37a7d6ce408c25
gauravgupta <grkml9630@gmail.com>
2022-06-21 15:50:31 +0530

    unionType

---
+++ Union/union.js
@@ -0,0 +1,11 @@
+// Uninon Type ( | ) ?
+// :- Union types decribes a value that can be one of several types.
+function combine(a, b) {
+    if (typeof a === 'number' && typeof b === 'number') {
+        return a + b;
+    }
+    else {
+        return a.toString() + b.toString();
+    }
+}
+console.log(combine(20, 30));
